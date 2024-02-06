39dc8f0c1d42727a2c057599703b13bc58d0ac49
Biodraken <159129039+Biodraken@users.noreply.github.com>
2024-02-06 11:05:36 +0100

    Add files via upload

---
+++ Pizzeria.Bollebygd/Index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
+  <title>Bollebygd Pizzeria.se</title>
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
+</head>
+
+<div>
+  <img class="LOGO" src="images/LOGO2.gif" alt="" />
+</div>
+
+<div class="topnav">
+  <div class="icons">
+    <a href="Index.php"><i class="bi bi-house"></i></a>
+    <a href="meny.php"><i class="bi bi-clipboard"></i></a>
+    <a href="kundvagn.php"><i class="bi bi-cart"></i></a>  <!--<div class="iconr"> -->
+    <a class="login" href="login.php"><i class="bi bi-person-fill"></i>Login</a>
+    <a class="signup" href="signup.php"><i class="bi bi-person-fill-add"></i>Sign up</a>
+    <!-- </div> -->
+  </div>
+</div>
+
+<div class="pizzor">
+  <div class="pizzaOne">
+    <img src="images/pizza-topping-ideas.jpg" alt="Avatar" class="image1">
+    <div class="overlay">
+      <div class="text1">Gör din egna pizza</div>
+    </div>
+  </div>
+
+  <div class="pizzaTwo">
+    <img src="images/pizza.bild.jpg" alt="Avatar" class="image2">
+    <div class="overlay">
+      <div class="text2">Hemleverans</div>
+    </div>
+  </div>
+
+  <div class="pizzaThree">
+    <img src="images/tropical-drinks.jpg" alt="Avatar" class="image3">
+    <div class="overlay">
+      <div class="text3">Enkel beställning</div>
+    </div>
+  </div>
+</div>
+
+<div class="inspiration">
+  <h3>Bara du vet hur du vill ha din pizza, så varför inte göra den som du vill ha den! </h3>
+</div>
+
+
+<div class="knapp">
+  <a href="meny.html">
+    <div>Gör din egna pizza här!</div>
+  </a>
+</div>
+</body>
+
+<footer>
+  <div class="kund">
+    <h3>Kundservice</h3>
+  </div>
+</footer>
+
+</html>
---
+++ Pizzeria.Bollebygd/meny.php
@@ -0,0 +1,36 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <title>Bollebygd Pizzeria.se</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  </head>
+
+  <body>
+    <img class="LOGO" src="images/LOGO.gif" alt="" />
+
+
+   <div class="topnav">
+      <a href="Index.html"><i class="bi bi-house"></i></a>
+      <a href="meny.html"><i class="bi bi-clipboard"></i></a>
+      <a href="kundvagn.html"><i class="bi bi-cart"></i></a>
+    </div>
+
+  
+
+  </body>
+
+  <footer>
+    <div class="kund">
+      <h3>Kundservice</h3>
+    </div>
+  </footer>
+</html>
